6e3a65d5b65be92f3373942eb9c65975c81cd446
Belendor <dwarf11@gmail.com>
2023-09-25 11:00:49 +0300

    update

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/internett', // Replace with your GitHub repository name
-  assetPrefix: '/internett/', // Replace with your GitHub repository name
 }
 
 module.exports = nextConfig
